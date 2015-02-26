a9c51ad8c863887c45375a1ea5f682f2ac1e5ca1
Anders Kiel Hovgaard <akhovg@gmail.com>
2015-02-26 17:06:23 +0100

    added test data of a few sequences, added simple readin of data file

--- src/main.cc
+++ src/main.cc
@@ -1,22 +1,39 @@
 #include "Distance.h"
 #include <string>
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 Distance dist;
 
 int main()
 {
-    std::string a = "actacgatgctagctggtcgatcgatgctag";
-    std::string b = "acgtcgtccctcgatcgctacgtggctaggagatc";
 
+    string a, b, c, d;
+    ifstream myfile ("data.txt");
+    if (myfile.is_open()) {
+        getline(myfile, a);
+        getline(myfile, b);
+        getline(myfile, c);
+        getline(myfile, d);
+        myfile.close();
+    } else
+        return 1;
 
-    int d2_dist  = dist.d2(a, b, 2);            // d2 distance using 2-grams
-    int lev_dist = dist.levenshtein(a, b);      // Levenshtein distance
+    //int d2_dist  = dist.d2(a, b, 2);            // d2 distance using 2-grams
+    //int lev_dist = dist.levenshtein(a, b);      // Levenshtein distance
 
-    std::cout << "a = " << a << std::endl;
-    std::cout << "b = " << b << std::endl;
-    std::cout << "d2 distance: " << d2_dist << std::endl;
-    std::cout << "Levenshtein distance: " << lev_dist << std::endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+    cout << "d = " << d << endl;
+
+    cout << "d2 distance(a,b): "            << dist.d2(a,b,3)           << endl;
+    cout << "Levenshtein distance(a,b): "   << dist.levenshtein(a,b)    << endl;
+    cout << endl;
+    cout << "d2 distance(c,d): "            << dist.d2(c,d,3)           << endl;
+    cout << "Levenshtein distance(c,d): "   << dist.levenshtein(c,d)    << endl;
 
     return 0;
 }
