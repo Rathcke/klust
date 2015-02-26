936162ae1ebf56c0f4c624286fa63a5520015233
Rathcke <Nikolaj.D.Rathcke@gmail.com>
2015-02-26 16:27:35 +0100

    Added comments

--- src/Distance.cc
+++ src/Distance.cc
@@ -12,16 +12,21 @@ int Distance::d2(string s, string t, int k) {
     int dist = 0;
     int v0[gramlen], v1[gramlen];
     int i;
+    // Initialize two array to count how many times each substring occurs in
+    // string
     for (i = 0; i < gramlen; i++) {
         v0[i] = 0;
         v1[i] = 0;
     }
+    // Amount of each substring in s
     for (i = 0; i <= slen-k; i++) {
         v0[gram_pos(s.substr(i,k))]++;
     }
+    // Amount of each substring in t
     for (i = 0; i <= tlen-k; i++) {
         v1[gram_pos(t.substr(i,k))]++;
     }
+    // Euclidian distance between the two arrays
     for (i = 0; i < gramlen; i++) {
         dist += pow(v0[i]-v1[i],2);
     }
@@ -31,6 +36,7 @@ int Distance::d2(string s, string t, int k) {
 int Distance::gram_pos(string s) {
     int slen = s.length();
     int cost = 0;
+    // Loop that calculates the index for a substring
     for (int i = slen - 1; i >= 0; i--) {
         switch (s[i]) {
             case 'a':
@@ -55,6 +61,7 @@ int Distance::gram_pos(string s) {
 int Distance::levenshtein(string s, string t) {
 	int slen = s.length();
 	int tlen = t.length();
+    // Trivial cases
 	if (slen == 0) {
 		return tlen;
 	}
@@ -66,6 +73,7 @@ int Distance::levenshtein(string s, string t) {
 	for (int i = 0; i < slen+1; i++) {
 		pcol[i] = i;
 	}
+    // Dynamic approach to calculate the distance between two strings
 	for (int i = 0; i < tlen; i++) {
 		col[0] = i+1;
 		for (int j = 0; j < slen; j++) {
