d70a6ed426efa6f23b08ec81886896c20da171d9
Rathcke <Nikolaj.D.Rathcke@gmail.com>
2015-03-13 16:25:27 +0100

    Added function computeKey

--- src/Distance.h
+++ src/Distance.h
@@ -9,12 +9,13 @@ class Distance
         Distance() {}
 
         static int d2(const std::string s, const std::string t, int k);
-        static int d2window(const std::string s, const std::string t, int k);
+        static bool d2window(const std::string s, const std::string t, int k, int threshold);
+        static std::vector<int> computeKey(const std::string& s, int k, int n);
         static int d2window_naive(std::string s, std::string t, int k);
         static int levenshtein(std::string s, std::string t);
         static int lev(std::string s, std::string t);
 
-        static void printDistMatrix(const std::string& filename, int k, int count);
+        static void printDistMatrix(const std::string& filename, int k, int count, int threshold);
 
     private:
         static int gram_pos(std::string s);
