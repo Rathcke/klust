a94e628233072f8f6cb7f08181d395cbc6ad2375
Anders Kiel Hovgaard <akhovg@gmail.com>
2015-05-21 16:05:44 +0200

    added sorting to performance test script

--- experiments/performance/test.sh
+++ experiments/performance/test.sh
@@ -4,12 +4,17 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 DATASET=$DIR/../../data/SILVA_119_SSURef_tax_silva.fasta
-
 KLUST=$DIR/../../src/klust
 
 for k in `seq 4 8`; do
   for t in `seq 0.75 0.05 0.95`; do
     $KLUST $DATASET "cts_${k}_${t}" "cls_${k}_${t}" -k $k -t $t \
       2>&1 | tee "klust_${k}_${t}"
+
+    $KLUST $DATASET "cts_${k}_${t}" "cls_${k}_${t}" -k $k -t $t \
+      --sort_incr 2>&1 | tee "klust_${k}_${t}_sort_incr"
+
+    $KLUST $DATASET "cts_${k}_${t}" "cls_${k}_${t}" -k $k -t $t \
+      --sort_decr 2>&1 | tee "klust_${k}_${t}_sort_decr"
   done
 done
