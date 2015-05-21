53b12ab7637407f0b0afb827610a23f31e60ceb2
Rathcke <Nikolaj.D.Rathcke@gmail.com>
2015-05-21 17:14:59 +0200

    Output from 75 testcases

--- experiments/performance/test.sh
+++ experiments/performance/test.sh
@@ -8,13 +8,13 @@ KLUST=$DIR/../../src/klust
 
 for k in `seq 4 8`; do
   for t in `seq 0.75 0.05 0.95`; do
-    $KLUST $DATASET "cts_${k}_${t}" "cls_${k}_${t}" -k $k -t $t \
+    $KLUST $DATASET "cts_${k}_${t}" "cls_${k}_${t}" -k $k -t $t -c 100000 \
       2>&1 | tee "klust_${k}_${t}"
 
-    $KLUST $DATASET "cts_${k}_${t}" "cls_${k}_${t}" -k $k -t $t \
+    $KLUST $DATASET "cts_${k}_${t}" "cls_${k}_${t}" -k $k -t $t -c 100000 \
       --sort_incr 2>&1 | tee "klust_${k}_${t}_sort_incr"
 
-    $KLUST $DATASET "cts_${k}_${t}" "cls_${k}_${t}" -k $k -t $t \
+    $KLUST $DATASET "cts_${k}_${t}" "cls_${k}_${t}" -k $k -t $t -c 100000 \
       --sort_decr 2>&1 | tee "klust_${k}_${t}_sort_decr"
   done
 done
