490d1938d116296f659560aaf6df0ae328056638
Rathcke <Nikolaj.D.Rathcke@gmail.com>
2015-05-24 22:06:06 +0200

    Updated bash script to generate output

--- experiments/performance/test.sh
+++ experiments/performance/test.sh
@@ -8,13 +8,20 @@ KLUST=$DIR/../../src/klust
 
 for k in `seq 4 8`; do
   for t in `seq 0.75 0.05 0.95`; do
-    $KLUST $DATASET "cts_${k}_${t}" "cls_${k}_${t}" -k $k -t $t -c 100000 \
-      2>&1 | tee "klust_${k}_${t}"
+    $KLUST $DATASET -k $k -t $t -c 100000 2>&1 | tee "klust_${k}_${t}"
 
-    $KLUST $DATASET "cts_${k}_${t}" "cls_${k}_${t}" -k $k -t $t -c 100000 \
-      --sort_incr 2>&1 | tee "klust_${k}_${t}_sort_incr"
+    $KLUST $DATASET -k $k -t $t -c 100000 -i 2>&1 | tee "klust_${k}_${t}_sort_incr"
 
-    $KLUST $DATASET "cts_${k}_${t}" "cls_${k}_${t}" -k $k -t $t -c 100000 \
-      --sort_decr 2>&1 | tee "klust_${k}_${t}_sort_decr"
+    $KLUST $DATASET -k $k -t $t -c 100000 -d 2>&1 | tee "klust_${k}_${t}_sort_decr"
+  done
+done
+
+for k in `seq 5 6`; do
+  for t in `seq 0.8 0.05 0.9`; do
+    $KLUST $DATASET -o "SILVA_FULL_${k}_${t}_cts" -u "SILVA_FULL_${k}_${t}_cls" \
+    	 -k $k -t $t  2>&1 | tee "SILVA_FULL_klust_${k}_${t}"
+
+    $KLUST $DATASET -o "SILVA_FULL_${k}_${t}_cts_incr" -u "SILVA_FULL_${k}_${t}_cls_incr" \
+    	 -k $k -t $t -i 2>&1 | tee "SILVA_FULL_klust_${k}_${t}_sort_incr"
   done
 done
