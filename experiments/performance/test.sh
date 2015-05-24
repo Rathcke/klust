9a73a6beda47673b1c816347d5c3d9140d94f6bd
Anders Kiel Hovgaard <akhovg@gmail.com>
2015-05-24 22:09:38 +0200

    removed tabs

--- experiments/performance/test.sh
+++ experiments/performance/test.sh
@@ -19,9 +19,9 @@ done
 for k in `seq 5 6`; do
   for t in `seq 0.8 0.05 0.9`; do
     $KLUST $DATASET -o "SILVA_FULL_${k}_${t}_cts" -u "SILVA_FULL_${k}_${t}_cls" \
-    	 -k $k -t $t  2>&1 | tee "SILVA_FULL_klust_${k}_${t}"
+      -k $k -t $t  2>&1 | tee "SILVA_FULL_klust_${k}_${t}"
 
     $KLUST $DATASET -o "SILVA_FULL_${k}_${t}_cts_incr" -u "SILVA_FULL_${k}_${t}_cls_incr" \
-    	 -k $k -t $t -i 2>&1 | tee "SILVA_FULL_klust_${k}_${t}_sort_incr"
+      -k $k -t $t -i 2>&1 | tee "SILVA_FULL_klust_${k}_${t}_sort_incr"
   done
 done
